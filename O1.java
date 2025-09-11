38c1db56d4dc98dd1b65a5c43016dbd09bb712c4
Daniel <Daniel.halaas@gmail.com>
2025-09-11 12:39:47 +0200

    Rettet etter kommentar

--- O1.java
+++ O1.java
@@ -4,37 +4,53 @@ public class O1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        System.out.print("Hva er din brutto inntekt: ");
+        
         int bruttoInntekt = scanner.nextInt();
-        double trinnSkatt = 0.0; // i desimal, ikke prosent
-        double skatteBelop;
-
-        final boolean TRINN_1 = (bruttoInntekt <= 306050);
-        final boolean TRINN_2 = (bruttoInntekt > 306050 && bruttoInntekt <= 697150);
-        final boolean TRINN_3 = (bruttoInntekt > 697150 && bruttoInntekt <= 942400);
-        final boolean TRINN_4 = (bruttoInntekt > 942400 && bruttoInntekt <= 1410750);
-        final boolean TRINN_5 = (bruttoInntekt > 1410750);
+        int trinn = sjekkTrinn(bruttoInntekt);
         
-        if (bruttoInntekt > 217400) {
-            if (TRINN_1) {
-                trinnSkatt = 0.017;
-            } else if (TRINN_2) {
-                trinnSkatt = 0.040;
-            } else if (TRINN_3) {
-                trinnSkatt = 0.137;
-            } else if (TRINN_4) {
-                trinnSkatt = 0.167;
-            } else {
-                trinnSkatt = 0.177;
-            }
-        } else {
-            return;
+        double skatteBelop = 0.0;
+
+        // velger "unaturlig" bruk av switch, pga fall-through
+        switch (trinn) {
+            case 5: 
+                skatteBelop += (bruttoInntekt - 1410750) * 0.177;
+                bruttoInntekt = 1410750;
+            case 4: 
+                skatteBelop += (bruttoInntekt - 942400) * 0.167;
+                bruttoInntekt = 942400;
+            case 3:
+                skatteBelop += (bruttoInntekt - 697150) * 0.137;
+                bruttoInntekt = 697150;
+            case 2:
+                skatteBelop += (bruttoInntekt - 306050) * 0.040;
+                bruttoInntekt = 306050;
+            case 1:
+                skatteBelop += (bruttoInntekt - 217400) * 0.017;
+                break;
+            case 0:
+                return;
         }
-
-        skatteBelop = bruttoInntekt * trinnSkatt;
         
-        System.out.println("Trinnskatt: " + trinnSkatt * 100 + "%");
         System.out.println("Skatt å betale: " + skatteBelop);
 
         scanner.close();
+    }
+
+    // hjelpefunksjon for å gjøre implementere switch
+    private static int sjekkTrinn(int inntekt) {
+        if (inntekt > 1410750) {
+            return 5;
+        } else if (inntekt > 942400) {
+            return 4;
+        } else if (inntekt > 697150) {
+            return 3;
+        } else if (inntekt > 306050) {
+            return 2;
+        } else if (inntekt > 217400) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
